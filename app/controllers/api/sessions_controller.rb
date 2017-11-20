ed380df851da756bdcdb3dce07a83dae6e3ec626
JeffreyMJordan <jeff2016@alumni.stanford.edu>
2017-11-20 11:30:21 -0800

    Adds logout api endpoints

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,2 +1,27 @@
 class Api::SessionsController < ApplicationController
+
+  def create 
+    @user = User.find_by_credentials(user_params[:username], user_params[:password])
+    if @user 
+      login!(@user)
+      render "api/users/show"
+    else 
+      render json: ["Invalid credentials"], status: 401
+    end 
+  end
+
+  def destroy 
+    if logged_in? 
+      logout! 
+      render json: ["Succesfully logged out"]
+    else 
+      render json: ["Must be logged in to logout"], status: 404
+    end
+  end
+
+
+  private 
+  def user_params 
+    params.require(:user).permit(:username, :password)
+  end
 end
