fc19fa199b78f3d3b36b1f4e94844fa902ece092
JeffreyMJordan <jeff2016@alumni.stanford.edu>
2017-11-20 11:01:43 -0800

    Adds auth methods to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,35 @@
 class User < ApplicationRecord
+  validates :username, :password, presence: true
+  validates :username, uniqueness: true 
+  validates :password, length: {minimum: 6, allow_nil: true}
+  attr_reader :password 
+  after_initialize :ensure_session_token 
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def ensure_session_token
+    self.session_token ||= SecureRandom.urlsafe_base64(16)
+  end
+
+  def reset_session_token 
+    self.session_token = SecureRandom.urlsafe_base64(16)
+    self.save! 
+  end
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by_username(username)
+    if user 
+      if BCrypt::Password.new(user.password_digest).is_password?(password)
+        return user 
+      else 
+        return nil 
+      end
+    else 
+      return nil 
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  namespace :api do 
+    resources :users, only: [:create]
+    resources :session, only: [:create, :destroy]
+  end
+
   root 'static_pages#root'
 end
