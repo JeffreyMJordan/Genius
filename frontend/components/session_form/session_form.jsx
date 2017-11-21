61a10bb91e7ed3cdf1f33df723c85d5f4e64e39f
JeffreyMJordan <jeff2016@alumni.stanford.edu>
2017-11-21 14:43:00 -0800

    Adds signup and login actions for session form

--- frontend/components/session_form/session_form.jsx
+++ frontend/components/session_form/session_form.jsx
@@ -3,25 +3,45 @@ import React from "react";
 class SessionForm extends React.Component{
   constructor(props){
     super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.state = {
+      username: "",
+      password: ""
+    };
+  }
+
+  handleSubmit(e){
+    e.preventDefault();
+    this.props.formSubmit(this.state);
+  }
+
+  handleChange(type){
+    return (e) => {
+      this.setState({[type]: e.target.value});
+      console.log(this.state);
+    };
   }
 
   render(){
-    
-    if(this.props.formType==="login"){
-      return (
-        <div> 
-          <h1>Login</h1>
-        </div>
-      );
-    }else{
-      return (
-        <div> 
-          <h1>Signup</h1>
-        </div>
-      );
-    }
+    let text = this.props.formType==="login"? "Sign in to Genius" : "Sign up for Genius";
+    let buttonText = this.props.formType==="login"? "Sign In" : "Sign Up";
+    return (
+      <div>
+        <h2>{text}</h2>
+        <form onSubmit={this.handleSubmit} action="">
+          <label htmlFor="">Username
+            <input type="text" onChange={this.handleChange('username')} value={this.state.username}/>
+          </label>
 
-    
+          <label htmlFor="">Password
+            <input type="password" onChange={this.handleChange('password')} value={this.state.password}/>
+          </label>
+
+          <input type="submit" value={buttonText}/>
+        </form>
+      </div>
+    );
+
   }
 }
 
