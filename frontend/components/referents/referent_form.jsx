7943ee780bdf467dbdbc2bdf3f45b6690e5bb210
JeffreyMJordan <jeff2016@alumni.stanford.edu>
2017-11-25 17:20:17 -0800

    Adds referent creation button

--- frontend/components/referents/referent_form.jsx
+++ frontend/components/referents/referent_form.jsx
@@ -3,11 +3,22 @@ import React from "react";
 class ReferentForm extends React.Component{
   constructor(props){
     super(props);
+    console.log(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(){
+    let ref = {};
+    ref["start_idx"] = this.props.match.params.startIdx;
+    ref["end_idx"] = this.props.match.params.endIdx;
+    ref["song_id"] = this.props.song.id;
+    ref["creator_id"] = this.props.currentUser.id;
+    this.props.createReferent(ref);
   }
 
   render(){
     return (
-      <h1>It's rendering</h1>
+      <button onClick={this.handleClick}>Create Referent</button>
     );
   }
 }
--- frontend/components/referents/referent_form_container.js
+++ frontend/components/referents/referent_form_container.js
@@ -1,12 +1,21 @@
 import {connect} from "react-redux";
 import ReferentForm from "./referent_form";
+import {createReferent} from "../../actions/referent_actions";
 
 const mapStateToProps = (state, ownProps) => {
-  return {};
+  let song = state.songs[ownProps.match.params.songId];
+  let referents = state.referents[song.id];
+  return {
+    song,
+    referents,
+    currentUser: state.session.currentUser
+  };
 };
 
-const mapDispatchToProps = (state, ownProps) => {
-  return {};
+const mapDispatchToProps = (dispatch, ownProps) => {
+  return {
+    createReferent: (ref) => dispatch(createReferent(ref))
+  };
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(ReferentForm);
