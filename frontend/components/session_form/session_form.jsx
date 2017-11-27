e9118a38981bcb7da236af7a8fe3bf0b5edf4721
JeffreyMJordan <jeff2016@alumni.stanford.edu>
2017-11-27 10:57:31 -0800

    Changes login and signup styling

--- frontend/components/session_form/session_form.jsx
+++ frontend/components/session_form/session_form.jsx
@@ -26,24 +26,23 @@ class SessionForm extends React.Component{
     let text = this.props.formType==="login"? "Sign in to Genius" : "Sign up for Genius";
     let buttonText = this.props.formType==="login"? "Sign In" : "Sign Up";
     return (
-      <div className="modal">
-        <div className="modal-screen"></div>
+      <div className="song-form">
+        {/* <div className="modal-screen"></div> */}
 
-        <div className="modal-form">
-        <Link className="x-button" to="/">X</Link>
+        
         <h2>{text}</h2>
         <form onSubmit={this.handleSubmit} action="">
-          <label htmlFor="">
-            <input className="text-form" type="text" placeholder="Username" onChange={this.handleChange('username')} value={this.state.username}/>
-          </label>
-          <br/>
-          <label htmlFor="">
-            <input className="text-form" type="password" placeholder="Password" onChange={this.handleChange('password')} value={this.state.password}/>
-          </label>
-          <br/>
-          <input type="submit" className="submit-button" value={buttonText}/>
+          
+          <h3>Username</h3>  
+          <input  type="text" placeholder="Username" onChange={this.handleChange('username')} value={this.state.username}/>
+          
+          
+          <h3>Password</h3>
+          <input  type="password" placeholder="Password" onChange={this.handleChange('password')} value={this.state.password}/>
+          
+          
+          <input className="song-submit" type="submit" value={buttonText}/>
         </form>
-        </div>
       </div>
     );
 
