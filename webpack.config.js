7f9afab657363ef6d3a743845242676335aed8f8
JeffreyMJordan <jeff2016@alumni.stanford.edu>
2017-12-03 18:49:45 -0800

    Changes name of entry file to prodigy

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ plugins = plugins.concat(
 // include plugins config
 module.exports = {
   context: __dirname,
-  entry: "./frontend/genius.jsx",
+  entry: "./frontend/prodigy.jsx",
   output: {
     path: path.resolve(__dirname, "app", "assets", "javascripts"),
     filename: "bundle.js"
