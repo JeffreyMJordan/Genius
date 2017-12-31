5a953dcccc632850a80fe02b360f07ec9b0ba798
JeffreyMJordan <jeff2016@alumni.stanford.edu>
2017-12-31 15:54:22 -0800

    Refactors album track list

--- frontend/components/albums/album_display_index.jsx
+++ frontend/components/albums/album_display_index.jsx
@@ -9,9 +9,17 @@ class AlbumDisplayIndex extends React.Component{
 
   render(){
     console.log(this.props.songs);
-    return (<ul>
-      {this.props.songs.map((song) =>  <AlbumDisplayIndexItem album={this.props.album} song={song}/>)}
-    </ul>);
+    return (
+      <div className="songs-index">
+      <ul>
+        <li>
+          <div className="songs-index-header">
+            <h3>Track List</h3>
+          </div>
+        </li>
+        {this.props.songs.map((song) =>  <AlbumDisplayIndexItem album={this.props.album} song={song}/>)}
+      </ul>
+    </div>);
   }
 }
 
