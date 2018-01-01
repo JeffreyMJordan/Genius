9bcc883d0130ccd814c919dc9ed1a614ba7b3edd
JeffreyMJordan <jeff2016@alumni.stanford.edu>
2017-12-31 16:00:24 -0800

    Fixes song index bug

--- frontend/components/songs/song_index_item.jsx
+++ frontend/components/songs/song_index_item.jsx
@@ -1,21 +1,26 @@
 import React from "react";
 import {Link} from "react-router-dom";
 export default ({song, album, artist}) => {
-  return (
-    <li className="song-index-item">
-      <Link to={`/songs/${song.id}`}>
-      <img className="song-index-item-image" src={album.image_url} alt="No album image :)"/>
-      <br/>
-      <div className="song-index-item-information">
-        <div className="song-index-item-title">
-          {song.title}
+  if(artist){
+    return (
+      <li className="song-index-item">
+        <Link to={`/songs/${song.id}`}>
+        <img className="song-index-item-image" src={album.image_url} alt="No album image :)"/>
+        <br/>
+        <div className="song-index-item-information">
+          <div className="song-index-item-title">
+            {song.title}
+          </div>
+          <br/>
+          {artist.name}
         </div>
         <br/>
-        {artist.name}
-      </div>
-      <br/>
-      </Link>
-      
-    </li>
-  );
+        </Link>
+        
+      </li>
+    );
+  }else{
+    return <li>placeholder</li>
+  }
+  
 };
